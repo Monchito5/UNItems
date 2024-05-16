437339461ea9a8944c294b3623c623832e2022a9
Moncho <91226432+Monchito5@users.noreply.github.com>
2024-05-16 13:49:46 -0600

    Update config.py
    sec

--- UNItemsApp/config.py
+++ UNItemsApp/config.py
@@ -12,9 +12,9 @@ class DevelompentConfig(config):
 
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 465
-    MAIL_USERNAME = "learntoapplication@gmail.com"
+    MAIL_USERNAME = "@gmail.com"
     MAIL_DEFAULT_SENDER = MAIL_USERNAME
-    MAIL_PASSWORD = "zpjpadmxcccqlztq"
+    MAIL_PASSWORD = ""
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
